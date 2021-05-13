79b53271c11a5cc2368c49019b3ea4afb661941b
loic <h-loic@orange.fr>
2021-05-13 23:33:37 +0200

    ajout pagination

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import { Menu, Dropdown } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import { MDBBtn } from "mdbreact";
+import { Pagination } from 'antd';
 
 import React from "react";
 
@@ -32,12 +33,18 @@ const menu = (
   </Menu>
 );
 
+function onShowSizeChange(current, pageSize) {
+  console.log(current, pageSize);
+}
+
 export default class NewsFeed extends React.Component {
   render()  {
     return (
       <div>
         <div class="row mw-100 m-0 p-0 mt-4">
-          <div class="col-lg-8"></div>
+          <div class="col-lg-8">
+            <h2 class="ml-5">Les derniers sondages</h2>
+          </div>
           <div class="col-lg-4 col-md-12 text-center">
             <Dropdown overlay={menu}>
               <a className="ant-dropdown-link font-weight-bold blue-color" onClick={e => e.preventDefault()}>
@@ -47,7 +54,7 @@ export default class NewsFeed extends React.Component {
           </div>
           <div class="col-lg-3"></div>
         </div>
-        <div class="row mw-100 m-0 p-0 mt-5">
+        <div class="row mw-100 m-0 p-0 mt-3">
           <div class="col-lg-4 col-md-12 mt-3 mb-3">
             <div class="card text-center">
               <div class="card-header bg-white">
@@ -168,6 +175,9 @@ export default class NewsFeed extends React.Component {
               </div>
             </div>
           </div>
+        </div>
+        <div class="text-center mt-2 mb-2">
+          <Pagination showSizeChanger onShowSizeChange={onShowSizeChange} defaultCurrent={1} total={35}/>
         </div>
         <Link to="/tableau-de-jp-baur">page surprise</Link>
       </div>
