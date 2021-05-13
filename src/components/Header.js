0426c4cd2bd0ffca0e28072c7dcd7bb39fd01394
loic <h-loic@orange.fr>
2021-05-13 13:16:19 +0200

    amélioration navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './../styles/header.css';
 
-import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import { MDBBtn } from "mdbreact";
+
+import React, { Fragment } from "react";
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
@@ -10,39 +15,56 @@ export default class Header extends React.Component {
     return (
       <header class="container mw-100 bg-white shadow-lg py-4 inline m-0 p-0">
         <div class="row">
-          <div class="col-1"></div>
-            <a class="col-2" href="#">
-                <img src="Sondeju_logo.png" width="150" height="40" class="d-inline-block align-top" alt=""></img>
-            </a>
 
-          <form class="col-6">
-            <div class="input-group">
-                <div class="input-group-btn search-panel">
-                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    	<span id="search_concept">Filter by</span> <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                      <li><a href="#contains">Contains</a></li>
-                      <li><a href="#its_equal">It's equal</a></li>
-                      <li><a href="#greather_than">Greather than</a></li>
-                      <li><a href="#less_than">Less than</a></li>
-                      <li class="divider"></li>
-                      <li><a href="#all">Anything</a></li>
-                    </ul>
-                </div>
-                <input type="hidden" name="search_param" value="all" id="search_param"></input>       
-                <input type="text" class="form-control" name="x" placeholder="Search term..."></input>
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-            </div>
-          </form>
+          <div class="col-lg-1"></div>
 
-          <button type="button" class="btn button-account col-2">Mon compte</button>
-          <div class="col-1"></div>
+          <a class="col-lg-2 mt-2 text-center" href="#">
+              <Logo></Logo>
+          </a>
+
+          <div class="col-lg-6 text-center">
+            <Searchbar></Searchbar>
+          </div>
+
+          <div class="col-lg-2 text-center">
+            <ButtonPage></ButtonPage>
+          </div>
+
+          <div class="col-lg-1"></div>
+
         </div>
       </header>
     );
   }
 }
 
+const Logo = () => {
+  return (
+    <Fragment>
+      <img src="Sondeju_logo.png" width="150" height="40" class="d-inline-block align-top" alt=""></img>
+    </Fragment>
+  );
+}
+
+const Searchbar = () => {
+  return (
+    <Fragment>
+      <div class="input-group rounded mt-2">
+        <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+        <span class="input-group-text border-0" id="search-addon">
+          <i class="fas fa-search"></i>
+        </span>
+      </div>
+    </Fragment>
+  );
+}
+
+const ButtonPage = () => {
+  return (
+    <Fragment>
+      <MDBBtn type="button" gradient="blue">
+                Mon compte
+      </MDBBtn>
+    </Fragment>
+  );
+}
