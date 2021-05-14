21ed0090eba6e226998f3b7f0abca310df048fc2
loic <h-loic@orange.fr>
2021-05-14 12:58:19 +0200

    ajout page account

---
+++ src/components/Account.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
+import './../styles/Account.css';
+
+import React from "react";
+
+export default class AccountContent extends React.Component {
+  render()  {
+    return (
+      <div>
+          <h1>profil</h1>
+      </div>
+    );
+  }
+}
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 
 import Layout from "../components/Layout";
+import AccountContent from "../components/Account"
 
 
 
@@ -10,6 +11,7 @@ export default  class Account extends React.Component {
   render()  {
     return (
       <Layout>
+        <AccountContent></AccountContent>
         <Link to="/tableau-de-jp-baur">page surprise</Link>
       </Layout>
       
