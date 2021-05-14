651294756c4e8cf5de391bc23aecfb4b7a4a1b43
loic <h-loic@orange.fr>
2021-05-14 15:17:59 +0200

    ajout footer

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,14 +4,52 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 import './../styles/Account.css';
+import { Link } from 'react-router-dom';
 
 import React from "react";
+
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+function callback(key) {
+  console.log(key);
+}
 
 export default class AccountContent extends React.Component {
   render()  {
     return (
       <div>
-          <h1>profil</h1>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-white">
+            <li class="breadcrumb-item"><Link to="/" class="blue-color font-weight-normal">Accueil</Link></li>
+            <li class="breadcrumb-item active" aria-current="page">Mon compte</li>
+          </ol>
+        </nav>
+
+        <h1 class="text-center font-weight-bold blue-color">Mon compte</h1>
+
+        <ul class="nav nav-tabs nav-justified mb-3 mt-5" id="ex1" role="tablist">
+          <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="mes-informations" data-mdb-toggle="tab" href="#ex3-tabs-1" role="tab" aria-controls="ex3-tabs-1" aria-selected="true">
+              Mes informations
+            </a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a class="nav-link" id="mes-sondages" data-mdb-toggle="tab" href="#ex3-tabs-2" role="tab" aria-controls="ex3-tabs-2" aria-selected="false">
+              Mes sondages
+              </a>
+          </li>
+        </ul>
+
+        <div class="tab-content" id="content">
+          <div class="tab-pane fade show active" id="ex3-tabs-1" role="tabpanel" aria-labelledby="ex3-tab-1">
+            Tab 3 content
+          </div>
+          <div class="tab-pane fade" id="ex3-tabs-2" role="tabpanel" aria-labelledby="ex3-tab-2">
+            Tab 2 content
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,73 @@ import React from "react";
 export default class Footer extends React.Component {
   render()  {
     return (
-      <footer>
-      </footer>
+      <footer class="text-center text-lg-start bg-grey text-muted mt-4 mw-100">
+ 
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+  </section>
+ 
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+     
+      <div class="row mt-3">
+      
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+        
+          <h6 class="text-uppercase fw-bold mb-4">
+          <img src="./logo.png" width="30" height="30"></img> Sondeju
+          </h6>
+          <p>
+            Projet réalisé dans le cadre de l'UV d'HM40 
+          </p>
+        </div>
+       
+       
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+       
+          <h6 class="text-uppercase fw-bold mb-4">
+            Navigation
+          </h6>
+          <p>
+            <a href="/" class="text-reset">Créer un sondage</a>
+          </p>
+          <p>
+            <a href="/" class="text-reset">Accéder au tutoriel</a>
+          </p>
+          <p>
+            <a href="/" class="text-reset">Sondage au hasard</a>
+          </p>
+          <p>
+            <a href="/" class="text-reset">Mon compte</a>
+          </p>
+        </div>
+       
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Belfort, 90000 FR</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+             info@sondeju.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 09 69 39 40 20</p>
+          <p><i class="fas fa-print me-3"></i> + 03 89 25 38 05</p>
+        </div>
+        
+      </div>
+      
+    </div>
+  </section>
+  
+  <div class="text-center p-4" >
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Sondeju.com</a>
+  </div>
+  
+</footer>
     );
   }
 }
