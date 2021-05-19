777cdf8bf0c3d03b9aaf855d76b9b379f0830978
Antoine RICHARD <antoine.richard68@gmail.com>
2021-05-19 17:08:26 +0200

    toolbar question fonctionnelle (drag and drop, supprimer, dupliquer)

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,7 +5,7 @@ export default class Question extends React.Component {
   render()  {
     return (
         <div className="">
-            test
+            {this.props.children}
         </div>
     );
   }
--- src/pages/CreateSurvey.js
+++ src/pages/CreateSurvey.js
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 import { Menu, Dropdown } from 'antd';
 import Question from "../components/Question";
 
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 
 export default class CreateSurvey extends React.Component {
@@ -15,40 +16,97 @@ export default class CreateSurvey extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      questions : [<Question/>, <Question/>]
+      i:0,
+      questions : []
     }
   }
   
   addQuestion(questionType) {
-    
+    this.state.i++;
+    this.setState({
+      questions: [...this.state.questions, <Question>{this.state.i}</Question>]
+    });
   }
   
-  removeQuestion(idQuestion) {
-    
+  duplicateQuestion(indexQuestion) {
+    this.setState({
+      questions: [...this.state.questions, this.state.questions[indexQuestion]]
+    });
   }
+  
+  removeQuestion(indexQuestion) {
+    this.setState({
+      questions: this.state.questions.filter((_, i) => i !== indexQuestion)
+    });
+  }
+  
+  moveQuestion(indexQuestion, newIndex) {
+    if(newIndex === -1 || newIndex === this.state.questions.length) return;
+    let questions = this.state.questions;
+    let question = questions.splice(indexQuestion, 1) [0];
+    questions.splice(newIndex, 0, question);
+    this.setState({
+      questions: questions
+    });
+  }
+  
+  handleDragEnd(dragObject) {
+    if(!dragObject.destination) return;
+    let indexSourceQuestion = dragObject.source.index;
+    let indexDestinationQuestion = dragObject.destination.index;
+    console.log(indexSourceQuestion, indexDestinationQuestion)
+    this.moveQuestion(indexSourceQuestion,indexDestinationQuestion);
+  }
+  
+  renderQuestions() {
+    return this.state.questions.map((question, index) => {
+      return (
+        <Draggable key={index} draggableId={"draggable-"+index} index={index}>
+          {(provided, snapshot) => (
+            <div className="container-question" ref={provided.innerRef} {...provided.draggableProps} 
+            style={{
+              ...provided.draggableProps.style,
+              boxShadow: snapshot.isDragging ? "0 0 .4rem black" : "none"
+              }} >
+              <div className="toolbar-question">
+                <span {...provided.dragHandleProps}><i class="fas fa-expand-arrows-alt"></i></span>
+                {index === 0 ? <span></span> : <span onClick={() => this.moveQuestion(index, index-1)}><i className="fas fa-arrow-up"></i></span>}
+                <span onClick={() => this.removeQuestion(index)}><i className="fas fa-trash-alt"></i></span>
+                <span onClick={() => this.duplicateQuestion(index)}><i className="fas fa-copy"></i></span>
+                {index === this.state.questions.length-1 ? <span></span> : <span onClick={() => this.moveQuestion(index, index+1)}><i className="fas fa-arrow-down"></i></span>}
+              </div>
+              {question}
+            </div>
+          )}
+        </Draggable>
+      );
+    });
+  }
+  
+
   
   menu() {
     return (
       <Menu>
-        <Menu.Item key="0">
-          <a href="#"><i class="fas fa-grip-lines"></i> Réponse courte</a>
+        <Menu.Item key="0" onClick={() => this.addQuestion("")}>
+          <span><i class="fas fa-grip-lines"></i> Réponse courte</span>
         </Menu.Item>
         <Menu.Item key="1">
-          <a href="#"><i class="fas fa-align-justify"></i> Réponse longue</a>
+          <span><i class="fas fa-align-justify"></i> Réponse longue</span>
         </Menu.Item>
         <Menu.Divider />
         <Menu.Item key="2">
-          <a href="#"><i class="far fa-circle"></i> Choix multiple</a>
+          <span><i class="far fa-circle"></i> Choix multiple</span>
         </Menu.Item>
         <Menu.Item key="3">
-          <a href="#"><i class="far fa-square"></i> Cases à cocher</a>
+          <span><i class="far fa-square"></i> Cases à cocher</span>
         </Menu.Item>
         <Menu.Item key="4">
-          <a href="#"><i class="fas fa-list"></i> Liste déroulante</a>
+          <span><i class="fas fa-list"></i> Liste déroulante</span>
         </Menu.Item>
         <Menu.Divider />
         <Menu.Item key="5">
-          <a href="#"><i class="fas fa-ruler-horizontal"></i> Echelle linéaire</a>
+          <span><i class="fas fa-ruler-horizontal"></i> Echelle linéaire</span>
         </Menu.Item>
       </Menu>
     );
@@ -68,11 +126,11 @@ export default class CreateSurvey extends React.Component {
         <h1 class="text-center font-weight-bold blue-color">Créer un sondage</h1>
         
         <section class="section-sondage mb-5 mt-5">
-            <h2 contentEditable="true">Titre du sondage</h2>
-            <p contentEditable="true">Entrer une description du sondage...</p>
+            <h2 contentEditable={true} suppressContentEditableWarning={true}>Titre du sondage</h2>
+            <p contentEditable={true} suppressContentEditableWarning={true}>Entrer une description du sondage...</p>
             
             <div className="text-center">
-              <Dropdown overlay={this.menu} trigger={['click']}>
+              <Dropdown overlay={this.menu.bind(this)} trigger={['click']}>
                 <a className="ant-dropdown-link btn btn-outline-info button-account" onClick={e => e.preventDefault()}>
                 <i class="fas fa-plus mr-1"></i> Ajouter une question
                 </a>
@@ -80,7 +138,15 @@ export default class CreateSurvey extends React.Component {
             </div>
             
             <div className="question text-center">
-              {this.state.questions}
+              <DragDropContext onDragEnd={(...props) => {this.handleDragEnd(props[0])}}>
+                <Droppable droppableId="droppable-1">
+                {(provided, _) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {this.renderQuestions()}
+                  </div>
+                )}
+                </Droppable>
+              </DragDropContext>
             </div>
             
             
