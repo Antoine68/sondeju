32edb714458dd44ca15e9dac45e2b70a0c32eefa
Antoine RICHARD <antoine.richard68@gmail.com>
2021-05-19 11:42:26 +0200

    dropdown créer sondage

--- src/pages/CreateSurvey.js
+++ src/pages/CreateSurvey.js
@@ -4,10 +4,41 @@ import { Link } from "react-router-dom";
 
 import Layout from "../components/Layout";
 
+import { Menu, Dropdown } from 'antd';
 
 
 
 export default class CreateSurvey extends React.Component {
+  
+  menu() {
+    return (
+      <Menu>
+        <Menu.Item key="0">
+          <a href="#"><i class="fas fa-grip-lines"></i> Réponse courte</a>
+        </Menu.Item>
+        <Menu.Item key="1">
+          <a href="#"><i class="fas fa-align-justify"></i> Réponse longue</a>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="2">
+          <a href="#"><i class="far fa-circle"></i> Choix multiple</a>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <a href="#"><i class="far fa-square"></i> Cases à cocher</a>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <a href="#"><i class="fas fa-list"></i> Liste déroulante</a>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="5">
+          <a href="#"><i class="fas fa-ruler-horizontal"></i> Echelle linéaire</a>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+  
+  
+  
   render()  {
     return (
       <Layout>
@@ -24,11 +55,14 @@ export default class CreateSurvey extends React.Component {
             <p contentEditable="true">Entrer une description du sondage...</p>
             
             <div className="text-center">
-                <button type="button" className="btn btn-outline-info button-account" data-mdb-ripple-color="dark">
-                <i class="fas fa-plus mr-1"></i>
-                Ajouter une question
-                </button>
-            </div>        
+              <Dropdown overlay={this.menu} trigger={['click']}>
+                <a className="ant-dropdown-link btn btn-outline-info button-account" onClick={e => e.preventDefault()}>
+                <i class="fas fa-plus mr-1"></i> Ajouter une question
+                </a>
+              </Dropdown>
+            </div>
+            
+            
         </section>
         
       </Layout>
