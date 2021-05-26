45c8e95a1f20b68162852b839a25b68eb69b2d79
loic <h-loic@orange.fr>
2021-05-26 11:36:13 +0200

    vérification formulaire inscription frontend

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,27 +9,51 @@ export default class RegisterComponent extends React.Component {
         super(props)
     
         this.onChangePseudo = this.onChangePseudo.bind(this);
-        this.onChangeMail = this.onChangeMail.bind(this);
+        this.onChangeAge = this.onChangeAge.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     
         this.state = {
         pseudo: '',
-        mail: '',
-        password: ''
+        age: '0',
+        password: '',
+        pseudoErreur: '0',
+        ageErreur: '0',
+        passwordErreur: '0'
         }
     }
 
     onChangePseudo(e) {
         this.setState({pseudo: e.target.value})
+        if (e.target.value == ""){
+            this.setState({pseudoErreur : 1})
+            document.getElementById("erreurPseudo").innerHTML = "renseigner un pseudo est obligatoire"
+        }else{
+            this.setState({pseudoErreur : 0})
+            document.getElementById("erreurPseudo").innerHTML = ""
+        }
     }
     
-    onChangeMail(e) {
-        this.setState({mail: e.target.value})
+    onChangeAge(e) {
+        this.setState({age: e.target.value})
+        if (e.target.value == "" || e.target.value < 0){
+            this.setState({ageErreur : 1})
+            document.getElementById("erreurAge").innerHTML = "renseigner votre âge est obligatoire et dois être valide"
+          }else{
+            this.setState({ageErreur : 0})
+            document.getElementById("erreurAge").innerHTML = ""
+          }
     }
     
     onChangePassword(e) {
         this.setState({password: e.target.value})
+        if (e.target.value == "" || e.target.value.length < 8){
+            this.setState({passwordErreur : 1})
+            document.getElementById("erreurPassword").innerHTML = "choisir un mot de passe est obligatoire et dois possédé au minimum 8 caractères"
+          }else{
+            this.setState({passwordErreur : 0})
+            document.getElementById("erreurPassword").innerHTML = ""
+          }
     }
 
     onSubmit(e) {
@@ -37,7 +61,7 @@ export default class RegisterComponent extends React.Component {
 
         const userObject = {
             pseudo: this.state.pseudo,
-            mail: this.state.mail,
+            age: this.state.age,
             password: this.state.password
         };
 
@@ -67,17 +91,23 @@ export default class RegisterComponent extends React.Component {
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="pseudo">Pseudonyme</label>
-                            <input  id="pseudo" class="form-control" onChange={this.onChangePseudo}/>
+                            <input  type="text" name="pseudo" class="form-control col-lg-12 col-sm-12" style={{ border: this.state.pseudoErreur == 1 ? '1px solid red' : '' }} 
+                            onChange={this.onChangePseudo} value={this.state.pseudo} />
+                            <p class="col-lg-12 col-sm-12 text-danger" id="erreurPseudo"></p>
                         </div>
 
                         <div class="form-outline mb-4">
-                            <label class="form-label" for="mail">Adresse mail</label>
-                            <input type="email" id="mail" class="form-control" onChange={this.onChangeMail}/>
+                            <label class="form-label" for="mail">Age</label>
+                            <input type="number" id="age" name="age" class="form-control col-lg-12 col-sm-12" 
+                            style={{ border: this.state.ageErreur == 1 ? '1px solid red' : '' }} onChange={this.onChangeAge} value={this.state.age}/>
+                            <p class="col-lg-12 col-sm-12 text-danger" id="erreurAge"></p>
                         </div>
 
                         <div class="form-outline mb-4">
                             <label class="form-label" for="password">Mot de passe</label>
-                            <input type="password" id="password" class="form-control" onChange={this.onChangePassword}/>
+                            <input type="password" id="password" name="password" class="form-control col-lg-12 col-sm-12" 
+                            style={{ border: this.state.passwordErreur == 1 ? '1px solid red' : '' }} onChange={this.onChangePassword} value={this.state.password}/>
+                            <p class="col-lg-12 col-sm-12 text-danger" id="erreurPassword"></p>
                         </div>
 
                         <button class="btn btn-primary btn-block bg-button" type="submit">S'inscrire</button>
