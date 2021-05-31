2c259da94adc10f781e71a45bdcd79377d452ad1
Antoine RICHARD <antoine.richard68@gmail.com>
2021-05-31 16:40:35 +0200

    modification fonction sanitize

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 export const sanitize = (html, allowLineBreak=false) => {
     let result = html;
     if(allowLineBreak) result = result.replace(/&nbsp;/g, '\n').replace(/\<br\s*[\/]?>/gi, '\n');
+    else result = result.replace(/\r?\n|\r/g, ' ');
     result = result.replace(/(<([^>]+)>)/gi, "");
     if(!result.trim()) result="";
     return result;
