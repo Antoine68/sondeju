1e2751dd72bcae2b3e664d31b738458a992782da
loic <h-loic@orange.fr>
2021-06-02 15:06:25 +0200

    ajout réponse input

--- src/components/Response/ResponseInput.js
+++ src/components/Response/ResponseInput.js
@@ -1,10 +1,37 @@
 import React from "react";
+import './../../styles/Response.css';
 
 export default class ResponseInput extends React.Component {
+
+  constructor(props){
+    super(props)
+    console.log(this.props)
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: this.props.response.questionResponses,
+    });
+  }
+
+
     
     render()  {
       return (
-        <div>
+        <div class="table-wrapper-scroll-y my-custom-scrollbar">
+          {
+            this.state.data.map((response) => {
+              if (response.value === ""){
+                return(<div></div>)
+              }
+              return (
+                <div class="bg-list p-2 mt-1 mr-2 rounded">{response.value}</div>
+              );
+            })
+          }
         </div>
       );
     }
