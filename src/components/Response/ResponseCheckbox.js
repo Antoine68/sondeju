fdc3a340be2c6ec5736373660074832c9ce52291
loic <h-loic@orange.fr>
2021-06-09 14:38:06 +0200

    réponse choix multiple

--- src/components/Response/ResponseCheckbox.js
+++ src/components/Response/ResponseCheckbox.js
@@ -1,11 +1,57 @@
 import React from "react";
+import Chart from "react-google-charts";
 
 export default class ResponseCheckbox extends React.Component {
     
-    render()  {
-      return (
-        <div>
-        </div>
-      );
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [
+        ['response', 'number']
+      ]
     }
   }
+
+  componentDidMount() {
+    let responseData = []
+    let isThere = 0 
+    this.props.response.questionResponses.map((response) => {
+      isThere = 0
+      responseData.map((data) => {
+        if (response.options[0] !== undefined){
+          response.options.map((option) => {
+            if (option.value == data[0]){
+              isThere = 1
+              data[1] += 1
+            }
+          })
+        }
+      })
+      if (response.options[0] !== undefined){
+        if(isThere === 0){
+          response.options.map((option) => {
+            responseData.push([option.value,1])
+          })
+        }
+      }
+    })
+    this.setState({ data: [...this.state.data, ...responseData ] })
+  }
+    
+  render()  {
+    return (
+      <div>
+        <Chart
+          width={'500px'}
+          height={'300px'}
+          chartType="PieChart"
+          loader={<div>Loading Chart</div>}
+          data={this.state.data}
+          options={{
+            sliceVisibilityThreshold: 0.2, // 20%
+          }}
+          />
+      </div>
+    );
+  }
+}
